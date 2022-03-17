6fb5537bae2577e2c4965ccdb529e5fa1bd0293c
amiralipaak <amiralipaak380@gmail.com>
2022-03-17 23:12:01 +0330

    added style to it and finished the javascript of it

--- scripts.js
+++ scripts.js
@@ -1,41 +1,59 @@
 const typingDiv = document.getElementById("typing");
-const text = `In both the East and the West`;
+const statsDiv = document.getElementById("stats");
+const startGameBtn = document.getElementById("startGame");
+const header = document.getElementById("header");
 
-const characters = text.split("").map((char) => {
-  const span = document.createElement("span");
-  span.innerText = char;
-  typingDiv.appendChild(span);
-  return span;
-});
+const pharagraphs = [
+  `The Internet has revolutionized the computer and communications world like nothing before.`,
+  `The invention of the telegraph, telephone, radio, and computer set the stage for this unprecedented integration of capabilities.`,
+  `In both the East and the West, coinage proper was preceded by more primitive currencies, nonmonetary or semi-monetary`,
+  `which survived into the historic age of true coins, and may have derived originally from the barter of cattle, implements, and the like.`,
+];
 
-let cursorIndex = 0;
-let cursorCharacter = characters[cursorIndex];
-cursorCharacter.classList.add("cursor");
+const startGame = () => {
+  startGameBtn.classList.add("hidden");
+  header.classList.add("hidden");
+  typingDiv.innerHTML = "";
+  statsDiv.innerHTML = "";
 
-let startTime = null;
-let endTime = null;
+  const text = pharagraphs[parseInt(Math.random() * pharagraphs.length)];
 
-const keyListener = document.addEventListener("keydown", ({ key }) => {
-  console.log(key);
-  if (!startTime) {
-    startTime = new Date();
-  }
-  if (key === cursorCharacter.innerText) {
-    cursorCharacter.classList.remove("cursor");
-    cursorCharacter.classList.add("done");
-    cursorCharacter = characters[++cursorIndex];
-  }
-  if (cursorIndex >= characters.length) {
-    endTime = new Date();
-    const delta = endTime - startTime;
-    const seconds = delta / 1000;
-    const minutes = seconds / 60;
-    const numberOfWords = text.split(" ").length;
-    const wps = numberOfWords / seconds;
-    const wpm = wps * 60.0;
-    document.getElementById("stats").innerText = `wpm = ${parseInt(wpm)}`;
-    document.removeEventListener("keydown", keyListener);
-    return;
-  }
+  const characters = text.split("").map((char) => {
+    const span = document.createElement("span");
+    span.innerText = char;
+    typingDiv.appendChild(span);
+    return span;
+  });
+
+  let cursorIndex = 0;
+  let cursorCharacter = characters[cursorIndex];
   cursorCharacter.classList.add("cursor");
-});
+
+  let startTime = null;
+
+  const keyListener = document.addEventListener("keydown", ({ key }) => {
+    console.log(key);
+    if (!startTime) {
+      startTime = new Date();
+    }
+    if (key === cursorCharacter.innerText) {
+      cursorCharacter.classList.remove("cursor");
+      cursorCharacter.classList.add("done");
+      cursorCharacter = characters[++cursorIndex];
+    }
+    if (cursorIndex >= characters.length) {
+      const endTime = new Date();
+      const delta = endTime - startTime;
+      const seconds = delta / 1000;
+      const minutes = seconds / 60;
+      const numberOfWords = text.split(" ").length;
+      const wps = numberOfWords / seconds;
+      const wpm = wps * 60.0;
+      document.getElementById("stats").innerText = `wpm = ${parseInt(wpm)}`;
+      document.removeEventListener("keydown", keyListener);
+      startGameBtn.classList.remove("hidden");
+      return;
+    }
+    cursorCharacter.classList.add("cursor");
+  });
+};
